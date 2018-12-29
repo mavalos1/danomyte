ca5cb380613ca6ede05f5d74736109d3ffea829f
Daniel Nguyen <nguyen.anh3@husky.neu.edu>
2018-12-29 18:33:37 +0100

    Use Gromnet

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,26 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  ResponsiveContext,
+} from 'grommet';
+import { 
+  Notification
+} from 'grommet-icons';
+import theme from './theme';
 
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+  
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showSideBar: false,
+    };
+
+    this.handleNotificationClick = this.handleNotificationClick.bind(this);
+  }
+
+  handleNotificationClick() {
+    const { showSideBar } = this.state;
+    this.setState({ showSideBar: !showSideBar });
+  }
   render() {
+    const { showSideBar } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grommet theme={theme} full>
+        <ResponsiveContext.Consumer>
+          {size => (
+            <Box fill>
+              <AppBar>
+                {size}
+                <Heading level='3' margin='none'>CORNER-STORE</Heading>
+                <Button 
+                  icon={<Notification />} 
+                  onClick={this.handleNotificationClick}
+                />
+              </AppBar>
+              <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+                <Box flex align='center' justify='center'>
+                  app body
+                </Box>
+                <Collapsible direction='horizontal' open={showSideBar}>
+                  <Box
+                    flex
+                    width='medium'
+                    background='light-2'
+                    elevation='small'
+                    align='center'
+                    justify='center'
+                  >
+                    sidebar
+                  </Box>
+                </Collapsible>
+              </Box>
+            </Box>
+          )}
+        </ResponsiveContext.Consumer>
+      </Grommet>
     );
   }
 }
---
+++ dashboard/src/theme/index.js
@@ -0,0 +1,13 @@
+export default {
+	global: {
+		colors: {
+			brand: '#EBBE60',
+			white: '#000000',
+		},
+		font: {
+			family: 'Roboto',
+			size: '14px',
+			height: '20px',
+		},
+	},
+};
